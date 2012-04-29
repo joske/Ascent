b7694cfb2b11b48340b75334b56aa48faf694b24
Jos Dehaes <jos.dehaes@gmail.com>
2012-04-29 23:17:27 +0200

    avoid warning

--- src/be/sourcery/MyActivity.java
+++ src/be/sourcery/MyActivity.java
@@ -1,5 +1,6 @@
 package be.sourcery;
 
+import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Build;
@@ -7,6 +8,7 @@ import android.os.Build;
 
 public abstract class MyActivity extends Activity {
 
+    @TargetApi(11)
     protected void setupActionBar() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             ActionBar actionBar = getActionBar();
