f4b5282ffdb2442967e214dd03b8934c10ff48c5
Jos Dehaes <jos.dehaes@gmail.com>
2021-03-31 23:06:13 +0200

    fix test

--- app/src/main/java/be/sourcery/ascent/CodecUtil.java
+++ app/src/main/java/be/sourcery/ascent/CodecUtil.java
@@ -30,7 +30,6 @@ public class CodecUtil {
     }
 
     public static Ascent decode(String line) {
-        Log.d(CodecUtil.class.getName(), "importing " + line);
         String[] strings = line.split("\t");
         if (strings.length == 10) {
             try {
